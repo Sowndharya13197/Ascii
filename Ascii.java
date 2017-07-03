20a40de3b87ba44d6baccd938f252934de093911
Sowndharya13197 <sowndharyagopal13@gmail.com>
2017-07-03 21:41:45 +0530

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class Ascii {
+    public static void main(String args[])
+    {
+    char i;
+    int a;
+    for(i='a';i<='z';i++)
+    {
+    a=(int)i;
+    System.out.println(a);
+    }
+    
+}}
